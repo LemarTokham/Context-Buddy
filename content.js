ea9afb40b7efcca1b0e55ef07dda69fd2ecc7f59
Lemar Tokham <ltokham@gmail.com>
2025-08-14 11:02:41 +0100

    Create popup to show on page

--- content.js
+++ content.js
@@ -1,9 +1,46 @@
-console.log("Texh Highlighter extension ready to go")
+// Script that runs on actual web pages
+console.log("Text Highlighter extension ready to go")
 
-document.addEventListener('mouseup', () => {
+const popup = document.createElement('div')
+popup.className = 'definition-popup hidden' // Starting the popup as hidden initially
+document.body.appendChild(popup)
+
+function showPopup(text, x, y){
+    popup.innerHTML = `
+        <strong>You highlighted:</strong><br>
+        "${text}"<br><br>
+        <em>Definition coming soon...</em>
+    `;
+
+    popup.style.left = x + 'px'
+    popup.style.top = (y + 20) + 'px'
+
+    popup.classList.remove('hidden')
+
+}
+
+function hidePopup() {
+    popup.classList.add('hidden')
+}
+
+
+document.addEventListener('mouseup', (e) => {
     const selectedText = window.getSelection().toString()
 
     if (selectedText.length > 0){
         console.log("Highlighted:" , selectedText)
+
+        const x = e.pageX
+        const y = e.pageY
+
+        showPopup(selectedText, x, y)
+
     }
 })
+
+
+document.addEventListener('mousedown', () => {
+    hidePopup()
+})
+// new password: ekmuGei7o
+// Mesasi@230_mexico
